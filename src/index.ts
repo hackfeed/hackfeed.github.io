94be0ba436e2643d665ea50e2f2035100d553ded
Sergey Kononenko <hackfeed@yandex.ru>
2020-06-25 15:54:04 +0300

    Update my flex Webpage with useful info

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Testing");
+const contactsBtns = document.querySelectorAll(".contacts img");
+
+for (const btn of contactsBtns) {
+  btn.addEventListener("mouseover", () => {
+    btn.classList.add("img_hovered");
+    btn.classList.remove("img_unhovered");
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.classList.add("img_unhovered");
+    btn.classList.remove("img_hovered");
+  });
+}
+
+const navLinks = document.querySelectorAll("li a");
+
+for (const link of navLinks) {
+  link.addEventListener("mouseover", () => {
+    link.classList.add("link_hovered");
+    link.classList.remove("link_unhovered");
+  });
+  link.addEventListener("mouseout", () => {
+    link.classList.add("link_unhovered");
+    link.classList.remove("link_hovered");
+  });
+}
